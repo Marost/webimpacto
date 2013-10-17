fc260b4c78b7a3d015f1983161d64720e3dbb969
Marco Lopez <mlopez@virtualsip.net>
2013-10-17 17:41:45 -0500

    Administrador: Cambio de nombre de carpeta en CkEditor para upload de imagenes

--- panel@impacto/js/plugins/ckeditor/config.js
+++ panel@impacto/js/plugins/ckeditor/config.js
@@ -4,10 +4,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 CKEDITOR.editorConfig = function(config) {
-   config.filebrowserBrowseUrl = '/panel@diario16/js/plugins/kcfinder/browse.php?type=files';
-   config.filebrowserImageBrowseUrl = '/panel@diario16/js/plugins/kcfinder/browse.php?type=images';
-   config.filebrowserFlashBrowseUrl = '/panel@diario16/js/plugins/kcfinder/browse.php?type=flash';
-   config.filebrowserUploadUrl = '/panel@diario16/js/plugins/kcfinder/upload.php?type=files';
-   config.filebrowserImageUploadUrl = '/panel@diario16/js/plugins/kcfinder/upload.php?type=images';
-   config.filebrowserFlashUploadUrl = '/panel@diario16/js/plugins/kcfinder/upload.php?type=flash';
+   config.filebrowserBrowseUrl = '/panel@impacto/js/plugins/kcfinder/browse.php?type=files';
+   config.filebrowserImageBrowseUrl = '/panel@impacto/js/plugins/kcfinder/browse.php?type=images';
+   config.filebrowserFlashBrowseUrl = '/panel@impacto/js/plugins/kcfinder/browse.php?type=flash';
+   config.filebrowserUploadUrl = '/panel@impacto/js/plugins/kcfinder/upload.php?type=files';
+   config.filebrowserImageUploadUrl = '/panel@impacto/js/plugins/kcfinder/upload.php?type=images';
+   config.filebrowserFlashUploadUrl = '/panel@impacto/js/plugins/kcfinder/upload.php?type=flash';
 };
