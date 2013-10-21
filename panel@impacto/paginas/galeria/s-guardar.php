e569fd83f689af51d16e025d8402fd4937bc57a4
Marco Lopez <mlopez@virtualsip.net>
2013-10-21 15:08:30 -0500

    Administrador: Agregando opcion de Galeria de fotos

--- panel@impacto/paginas/galeria/s-guardar.php
+++ panel@impacto/paginas/galeria/s-guardar.php
@@ -6,9 +6,34 @@ require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
 
 //DECLARACION DE VARIABLES
 $nombre=$_POST["nombre"];
+$url=getUrlAmigable(eliminarTextoURL($nombre));
+$contenido=$_POST["contenido"];
+
+//FECHA Y HORA
+$pub_fecha=$_POST["pub_fecha"];
+$pub_hora=$_POST["pub_hora"];
+$fecha_publicacion=$pub_fecha." ".$pub_hora;
+$publicar=1;
+
+//SUBIR IMAGEN
+$upload_imagen=$_POST["uploader_galeria_0_tmpname"];
+
+//IMAGEN
+$cont=0;
+while($_POST['uploader_galeria_'.$cont.'_tmpname']<>""){
+	$imagen_fechaPub=$fechaActual;
+	$imagen=$_POST['uploader_galeria_'.$cont.'_tmpname'];
+	$thumb{$cont}=PhpThumbFactory::create("../../../imagenes/galeria/".$imagen_carpeta."".$imagen."");
+	$thumb{$cont}->adaptiveResize(96,72);
+	$thumb{$cont}->save("../../../imagenes/galeria/".$imagen_carpeta."thumb/".$imagen."", "jpg");
+	mysql_query("INSERT INTO ".$tabla_suf."_galeria_slide(imagen, imagen_carpeta, orden, noticia, fecha_publicacion) 
+		VALUES ('$imagen', '$imagen_carpeta', $cont_img, $noticia, '$imagen_fechaPub')",$conexion);
+	$cont++;
+}
 
 //INSERTANDO DATOS
-$rst_guardar=mysql_query("INSERT INTO ".$tabla_suf."_posicion_fija (posicion) VALUES('$nombre');",$conexion);
+$rst_guardar=mysql_query("INSERT INTO ".$tabla_suf."_galeria (url, titulo, contenido, fecha_publicacion, publicar) 
+	VALUES('$url', '".htmlspecialchars($nombre)."', '$contenido', '$fecha_publicacion', $publicar);",$conexion);
 
 if (mysql_errno()!=0){
 	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
