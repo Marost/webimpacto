38545db0dd6c54b79bfa659469b7690e1c9f01f7
MARCO LOPEZ <mlopez@virtualsip.net>
2013-10-20 22:57:23 -0500

    Administrador: Agregando opcion de Videos

--- panel@impacto/paginas/videos/s-editar.php
+++ panel@impacto/paginas/videos/s-editar.php
@@ -7,17 +7,40 @@ require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
 //DECLARACION DE VARIABLES
 $nota_id=$_REQUEST["id"];
 $titulo=$_POST["titulo"];
+$url=getUrlAmigable(eliminarTextoURL($titulo));
 $video=$_POST["video"];
+$publicar=1;
+
+//FECHA
 $pub_fecha=$_POST["pub_fecha"];
 $pub_hora=$_POST["pub_hora"];
 $fecha_publicacion=$pub_fecha." ".$pub_hora;
+
+//SUBIR IMAGEN
+$upload_imagen=$_POST["uploader_0_tmpname"];
+
+//IMAGEN
+if($upload_imagen<>""){
+	$imagen=$_POST["uploader_0_tmpname"];
+	$imagen_carpeta=fechaCarpeta()."/";	
+	$thumb=PhpThumbFactory::create("../../../imagenes/upload/".$imagen_carpeta."".$imagen."");
+	$thumb->adaptiveResize(770,464);
+	$thumb->save("../../../imagenes/upload/".$imagen_carpeta."thumb/".$imagen."", "jpg");
+}else{
+	$imagen=$_POST["imagen"];
+	$imagen_carpeta=$_POST["imagen_carpeta"];	
+	$thumb=PhpThumbFactory::create("../../../imagenes/upload/".$imagen_carpeta."".$imagen."");
+	$thumb->adaptiveResize(770,464);
+	$thumb->save("../../../imagenes/upload/".$imagen_carpeta."thumb/".$imagen."", "jpg");
+}
 
 //PUBLICAR
 if ($_POST["publicar"]<>""){ $publicar=$_POST["publicar"]; }else{ $publicar=0; }
 
 //INSERTANDO DATOS
-$rst_guardar=mysql_query("UPDATE ".$tabla_suf."_videos SET titulo='".htmlspecialchars($titulo)."', youtube='$video', 
-	fecha_publicacion='$fecha_publicacion', publicar=$publicar WHERE id=$nota_id;", $conexion);
+$rst_guardar=mysql_query("UPDATE ".$tabla_suf."_videos SET titulo='".htmlspecialchars($titulo)."', video='$video',
+	imagen='$imagen', 
+	imagen_carpeta='$imagen_carpeta', fecha_publicacion='$fecha_publicacion', publicar=$publicar WHERE id=$nota_id;", $conexion);
 
 if (mysql_errno()!=0){
 	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
--- panel@impacto/paginas/videos/s-guardar.php
+++ panel@impacto/paginas/videos/s-guardar.php
@@ -6,15 +6,33 @@ require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
 
 //DECLARACION DE VARIABLES
 $titulo=$_POST["titulo"];
+$url=getUrlAmigable(eliminarTextoURL($titulo));
 $video=$_POST["video"];
+$publicar=1;
+
+//FECHA
 $pub_fecha=$_POST["pub_fecha"];
 $pub_hora=$_POST["pub_hora"];
 $fecha_publicacion=$pub_fecha." ".$pub_hora;
-$publicar=1;
+
+//SUBIR IMAGEN
+$upload_imagen=$_POST["uploader_0_tmpname"];
+
+//IMAGEN
+if($upload_imagen<>""){
+	$imagen=$upload_imagen;
+	$imagen_carpeta=fechaCarpeta()."/";	
+	$mostrar_imagen=1;
+	$thumb=PhpThumbFactory::create("../../../imagenes/upload/".$imagen_carpeta."".$imagen."");
+	$thumb->adaptiveResize(770,464);
+	$thumb->save("../../../imagenes/upload/".$imagen_carpeta."thumb/".$imagen."", "jpg");
+}else{
+	$imagen=""; $imagen_carpeta="";
+}
 
 //INSERTANDO DATOS
-$rst_guardar=mysql_query("INSERT INTO ".$tabla_suf."_videos (titulo, youtube, fecha_publicacion, publicar) 
-	VALUES('".htmlspecialchars($titulo)."', '$video', '$fecha_publicacion', $publicar);",$conexion);
+$rst_guardar=mysql_query("INSERT INTO ".$tabla_suf."_videos (url, titulo, imagen, imagen_carpeta, video, fecha_publicacion, publicar) 
+	VALUES('$url', '".htmlspecialchars($titulo)."', '$imagen', '$imagen_carpeta', '$video', '$fecha_publicacion', $publicar);",$conexion);
 
 if (mysql_errno()!=0){
 	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
