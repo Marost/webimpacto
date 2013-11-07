330eb6e9a1947d43620e3024a171be7b1e3b9704
Marco Lopez <mlopez@virtualsip.net>
2013-11-07 17:52:51 -0500

    Home: Cambio de imagen y slide

--- w-slider.php
+++ w-slider.php
@@ -6,65 +6,58 @@
                 <ul>
                     
                     <li data-transition="papercut" data-slotamount="15" data-masterspeed="300" data-delay="9400">
-                        <img src="http://impacto.marostsac.com/imagenes/slide/foto1.png" >
+                        <img src="<?php echo $web; ?>imagenes/slide/slide1.jpg" >
                                                 
                         <div class="caption very_big_white lfl stl" data-x="0" data-y="100" data-speed="300" data-start="500" 
                         data-easing="easeOutExpo" data-end="8800" data-endspeed="300" data-endeasing="easeInSine" >
-                        Por los caminos de la Biblia</div>
+                        El fruto del justo es árbol de vida</div>
 
                         <div class="caption big_white lfl stl" data-x="0" data-y="150" data-speed="300" data-start="800" 
                         data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        Un viaje, en los cuales nos estaremos encontrando</div>
-
-                        <div class="caption big_white lfl stl" data-x="0" data-y="180" data-speed="300" data-start="800" 
-                        data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        continuamente en cada libro con nuestro Señor Jesucristo,</div>
-
-                        <div class="caption big_white lfl stl" data-x="0" data-y="210" data-speed="300" data-start="800" 
-                        data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        a quien estaremos saludando y reverenciando.</div>
+                        Proverbios 11:30</div>
                                                 
                     </li>
                     
                     <li data-transition="papercut" data-slotamount="15" data-masterspeed="300" data-delay="9400">
-                        <img src="http://impacto.marostsac.com/imagenes/slide/foto2.jpg" >
+                        <img src="<?php echo $web; ?>imagenes/slide/slide2.jpg" >
                                                 
                         <div class="caption very_big_white lfl stl" data-x="0" data-y="100" data-speed="300" data-start="500" 
                         data-easing="easeOutExpo" data-end="8800" data-endspeed="300" data-endeasing="easeInSine" >
-                        Qué tiene que ver el desorden con la creatividad</div>
+                        ¿Cuál es la edad de la tierra?</div>
 
                         <div class="caption big_white lfl stl" data-x="0" data-y="150" data-speed="300" data-start="800" 
                         data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        Al momento de trabajar o estudiar, las condiciones en las que</div>
+                        la Biblia enseña que la tierra tiene aproximadamente</div>
 
                         <div class="caption big_white lfl stl" data-x="0" data-y="180" data-speed="300" data-start="800" 
                         data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        se encuentra nuestro escritorio podrían tener un efecto</div>
-
-                        <div class="caption big_white lfl stl" data-x="0" data-y="210" data-speed="300" data-start="800" 
-                        data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        fundamental en nuestro desempeño.</div>
+                        6 mil años de edad</div>
                                                 
                     </li>
 
                     <li data-transition="papercut" data-slotamount="15" data-masterspeed="300" data-delay="9400">
-                        <img src="http://impacto.marostsac.com/imagenes/slide/foto3.jpg" >
+                        <img src="<?php echo $web; ?>imagenes/slide/slide3.jpg" >
                                                 
                         <div class="caption very_big_white lfl stl" data-x="0" data-y="100" data-speed="300" data-start="500" 
                         data-easing="easeOutExpo" data-end="8800" data-endspeed="300" data-endeasing="easeInSine" >
-                        Abriendo los oídos a Jesús</div>
+                        Lávame, y seré más blanco que la nieve</div>
 
                         <div class="caption big_white lfl stl" data-x="0" data-y="150" data-speed="300" data-start="800" 
                         data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        Jesús se acercó a lo irreparable, a lo olvidado, a lo adolorido.</div>
+                        Salmos 51:7</div>
+                                                
+                    </li>
 
-                        <div class="caption big_white lfl stl" data-x="0" data-y="180" data-speed="300" data-start="800" 
-                        data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        Dos ciegos que no tenían visión pero tenían oído cuando oyeron que Jesús pasaba,</div>
+                    <li data-transition="papercut" data-slotamount="15" data-masterspeed="300" data-delay="9400">
+                        <img src="<?php echo $web; ?>imagenes/slide/slide4.jpg" >
+                                                
+                        <div class="caption very_big_white lfl stl" data-x="0" data-y="100" data-speed="300" data-start="500" 
+                        data-easing="easeOutExpo" data-end="8800" data-endspeed="300" data-endeasing="easeInSine" >
+                        Conspiración contra la Biblia</div>
 
-                        <div class="caption big_white lfl stl" data-x="0" data-y="210" data-speed="300" data-start="800" 
+                        <div class="caption big_white lfl stl" data-x="0" data-y="150" data-speed="300" data-start="800" 
                         data-easing="easeOutExpo" data-end="9100" data-endspeed="300" data-endeasing="easeInSine" >
-                        empezaron a usar lo que tenían, Dios puede obrar cuando usamos lo que tenemos.</div>
+                        Apostasía de la Nueva Versión Internacional</div>
                                                 
                     </li>
             
