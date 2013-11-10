a7f564f8cf6819604dac1119ea708197a900ee73
Marco Lopez <mlopez@virtualsip.net>
2013-11-09 23:04:49 -0500

    Home: Widget LikeBox de Facebook

---
+++ w-facebook.php
@@ -0,0 +1,18 @@
+<aside>
+                            
+    <div class="sidebar">
+
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=563386783735798";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
+		<div class="fb-like-box" data-href="https://www.facebook.com/impactoevangelistico" data-width="290px" data-height="350px" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+
+	</div>
+	
+</aside>
