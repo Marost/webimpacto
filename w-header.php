18698c94139bfebaafe7986a1212798cdc49c3f4
Marco Lopez <mlopez@marostdevelopers.com>
2014-07-03 22:26:47 -0500

    Home: enlace de Slider Superior con BD

--- w-header.php
+++ w-header.php
@@ -2,6 +2,9 @@
 //CATEGORIAS
 $rst_menu=mysql_query("SELECT * FROM iev_noticia_categoria WHERE id<>11 AND id<>12 ORDER BY orden DESC", $conexion);
 
+//SLIDER SUPERIOR
+$rst_sliderSup=mysql_query("SELECT * FROM iev_slide_superior ORDER BY orden ASC", $conexion);
+
 ?>
 <div class="header-slider-opt">
 
@@ -49,7 +52,7 @@ $rst_menu=mysql_query("SELECT * FROM iev_noticia_categoria WHERE id<>11 AND id<>
                         <li><a href="/" title="">Inicio</a></li>
                         <li><a href="categoria/11/portada" title="">Portada</a></li>
                         <li><a href="categoria/12/noticias" title="">Noticias</a></li>
-                        <li><a href="editorial" title="">Editorial</a></li>
+                        <li><a href="edicion-anterior-es" title="">Ediciones</a></li>
                         <?php while($fila_menu=mysql_fetch_array($rst_menu)){
                                 $menu_id=$fila_menu["id"];
                                 $menu_url=$fila_menu["url"];
@@ -66,18 +69,29 @@ $rst_menu=mysql_query("SELECT * FROM iev_noticia_categoria WHERE id<>11 AND id<>
 
     </div> <!-- FIN DE OPCIONES DE HEADER -->
 
+    <?php if($sc_slider==true){ ?>
     <section id="slider">
             
         <div class="tp-banner-container">
                 <div class="tp-banner">
                     <ul>
 
+                        <?php while($fila_sliderSup=mysql_fetch_array($rst_sliderSup)){
+                                $SliderSup_titulo=$fila_sliderSup["titulo"];
+                                $SliderSup_contenido=$fila_sliderSup["contenido"];
+                                $SliderSup_imagen=$fila_sliderSup["imagen"];
+                                $SliderSup_imagen_carpeta=$fila_sliderSup["imagen_carpeta"];
+
+                                //URL
+                                $SliderSup_UrlImg=$web."imagenes/slide/".$SliderSup_imagen_carpeta."".$SliderSup_imagen;
+                        ?>
+
                         <li data-transition="fade" data-slotamount="15" data-masterspeed="500" data-delay="9400">
                     
-                            <img src="/imagenes/slide/slide1.jpg" data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+                            <img src="<?php echo $SliderSup_UrlImg; ?>" data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
                                                     
                             <div class="tp-caption slider_titulo"
-                                data-x="100"
+                                data-x="0"
                                 data-y="550"
                                 data-start="1000"
                                 data-speed="600"
@@ -85,11 +99,11 @@ $rst_menu=mysql_query("SELECT * FROM iev_noticia_categoria WHERE id<>11 AND id<>
                                 data-end="9000"
                                 data-endspeed="600"
                                 data-endeasing="Linear.easeNone"
-                                >El maravilloso universo
+                                ><?php echo $SliderSup_titulo; ?>
                             </div>
 
                             <div class="tp-caption slider_contenido"
-                                data-x="100"
+                                data-x="0"
                                 data-y="590"
 
                                 data-start="1000"
@@ -99,101 +113,12 @@ $rst_menu=mysql_query("SELECT * FROM iev_noticia_categoria WHERE id<>11 AND id<>
                                 data-end="9000"
                                 data-endspeed="600"
                                 data-endeasing="Linear.easeNone"
-                                >Claro ejemplo del poder del Creador
+                                ><p><?php echo $SliderSup_contenido; ?></p>
                             </div>
                                                     
                         </li>
-                        
-                        <li data-transition="fade" data-slotamount="15" data-masterspeed="300" data-delay="9400">
-                            <img src="/imagenes/slide/slide2.jpg" >                                                    
 
-                            <div class="tp-caption slider_titulo"
-                                data-x="100"
-                                data-y="550"
-                                data-start="1000"
-                                data-speed="600"
-                                data-easing="Linear.easeNone"
-                                data-end="9000"
-                                data-endspeed="600"
-                                data-endeasing="Linear.easeNone"
-                                >Los libros Apócrifos
-                            </div>
-
-                            <div class="tp-caption slider_contenido"
-                                data-x="100"
-                                data-y="590"
-
-                                data-start="1000"
-                                data-speed="600"
-                                data-easing="Linear.easeNone"
-
-                                data-end="9000"
-                                data-endspeed="600"
-                                data-endeasing="Linear.easeNone"
-                                >¿Tiene la Iglesia Romana y las Sociedades Bíblicas algún argumento de peso a <br> favor de los libros apócrifos?
-                            </div>
-
-                        </li>
-                        
-                        <li data-transition="fade" data-slotamount="15" data-masterspeed="300" data-delay="9400">
-                            <img src="/imagenes/slide/slide3.jpg" >
-
-                            <div class="tp-caption slider_titulo"
-                                data-x="100"
-                                data-y="550"
-                                data-start="1000"
-                                data-speed="600"
-                                data-easing="Linear.easeNone"
-                                data-end="9000"
-                                data-endspeed="600"
-                                data-endeasing="Linear.easeNone"
-                                >40 años preso y Dios lo liberó
-                            </div>
-
-                            <div class="tp-caption slider_contenido"
-                                data-x="100"
-                                data-y="590"
-
-                                data-start="1000"
-                                data-speed="600"
-                                data-easing="Linear.easeNone"
-
-                                data-end="9000"
-                                data-endspeed="600"
-                                data-endeasing="Linear.easeNone"
-                                >Juan Carlos Cedillo fue un temido asaltante mas Dios extendió sus manos para rescatarlo.
-                            </div>
-
-                        </li>
-
-                        <li data-transition="fade" data-slotamount="15" data-masterspeed="300" data-delay="9400">
-                            <img src="/imagenes/slide/slide4.jpg" >
-
-                            <div class="tp-caption slider_titulo"
-                                data-x="100"
-                                data-y="550"
-                                data-start="1000"
-                                data-speed="600"
-                                data-easing="Linear.easeNone"
-                                data-end="9000"
-                                data-endspeed="600"
-                                data-endeasing="Linear.easeNone"
-                                >El Matrimonio, Institución Divina
-                            </div>
-
-                            <div class="tp-caption slider_contenido"
-                                data-x="100"
-                                data-y="590"
-
-                                data-start="1000"
-                                data-speed="600"
-                                data-easing="Linear.easeNone"
-
-                                data-end="9000"
-                                data-endspeed="600"
-                                data-endeasing="Linear.easeNone"
-                                >“Y dijo Jehová Dios: No es bueno que el hombre esté solo; le haré ayuda idónea para él… <br> Jehová Dios… hizo una mujer, y la trajo al hombre” (Génesis 2:18-24).
-                            </div>
+                        <?php } ?>
 
                         </li>
                 
@@ -206,5 +131,6 @@ $rst_menu=mysql_query("SELECT * FROM iev_noticia_categoria WHERE id<>11 AND id<>
             </div>
 
     </section>
+    <?php } ?>
 
 </div> <!-- FIN OPCIONES: SLIDER - HEADER -->
