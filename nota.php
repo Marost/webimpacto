a0e92e9b93dbedeb24263819948a71e30fee3859
Marco Lopez <mlopez@marostdevelopers.com>
2014-07-03 22:27:22 -0500

    Home: identificador de header en nota

--- nota.php
+++ nota.php
@@ -68,13 +68,11 @@ $url_imagen=$web."imagenes/upload/".$noticia_imagen_carpeta."".$noticia_imagen;
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <header>
+        <header id="interno">
 
             <?php require_once("w-header.php"); ?>
             
         </header>
-
-        <?php require_once("w-slider.php"); ?>
 
         <section id="news">
             
