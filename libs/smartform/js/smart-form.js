497747f4f69ff919ed79da18daab7754289e80b7
Marco Lopez <mlopez@marostdevelopers.com>
2014-07-31 12:58:48 -0500

    libreria smartform

---
+++ libs/smartform/js/smart-form.js
@@ -0,0 +1,106 @@
+	$(function() {
+			   
+				/* @reload captcha
+				------------------------------------------- */			   
+				function reloadCaptcha(){
+					$("#captcha").attr("src","libs/smartform/php/captcha.php?r=" + Math.random());
+				}
+				
+				$('.captcode').click(function(e){
+					e.preventDefault();
+					reloadCaptcha();
+				});				
+			   
+				$( "#smart-form" ).validate({
+				
+						/* @validation states + elements 
+						------------------------------------------- */
+						errorClass: "state-error",
+						validClass: "state-success",
+						errorElement: "em",
+						onkeyup: false,
+						onclick: false,
+						
+						/* @validation rules 
+						------------------------------------------ */
+						rules: {
+								sendername: {
+										required: true,
+										minlength: 2
+								},
+								sendersubject: {
+										required: true,
+										minlength: 4
+								},								
+								sendermessage: {
+										required: true,
+										minlength: 10,
+                                        maxlength: 100
+								},
+								securitycode:{
+										required:true
+								}
+						},
+						
+					
+						messages:{
+								sendername: {
+										required: 'Ingresa tu nombre',
+										minlength: 'Mínimo 2 caracteres'
+								},
+								sendersubject: {
+										required: 'Ingresa tu país',
+										minlength: 'Mínimo 2 caracteres'
+								},														
+								sendermessage: {
+										required: 'Ingresa tu mensaje',
+										minlength: 'Mínimo 10 caracteres'
+								},															
+								securitycode:{
+										required: 'Ingresa el código'
+								}
+						},
+
+						/* @validation highlighting + error placement  
+						---------------------------------------------------- */
+						highlight: function(element, errorClass, validClass) {
+								$(element).closest('.field').addClass(errorClass).removeClass(validClass);
+						},
+						unhighlight: function(element, errorClass, validClass) {
+								$(element).closest('.field').removeClass(errorClass).addClass(validClass);
+						},
+						errorPlacement: function(error, element) {
+						   if (element.is(":radio") || element.is(":checkbox")) {
+									element.closest('.option-group').after(error);
+						   } else {
+									error.insertAfter(element.parent());
+						   }
+						},
+						
+						/* @ajax form submition 
+						---------------------------------------------------- */						
+						submitHandler:function(form) {
+							$(form).ajaxSubmit({
+								    target:'.result',			   
+									beforeSubmit:function(){ 
+											$('.form-footer').addClass('progress');
+									},
+									error:function(){
+											$('.form-footer').removeClass('progress');
+									},
+									 success:function(){
+											$('.form-footer').removeClass('progress');
+											$('.alert-success').show().delay(10000).fadeOut();
+											$('.field').removeClass("state-error, state-success");
+											if( $('.alert-error').length == 0){
+												$('#smart-form').resetForm();
+												reloadCaptcha();
+											}
+									 }
+							  });
+						}
+						// end submitHandler:
+				});		
+		
+	});				
+    
---
+++ libs/smartform/php/smartprocess.php
@@ -0,0 +1,98 @@
+<?php
+require_once("../../../panel@impacto/conexion/conexion.php");
+require_once("../../../panel@impacto/conexion/funciones.php");
+
+	if (!isset($_SESSION)) session_start(); 
+	if(!$_POST) exit;
+	
+	// Enter your name or company name below
+	$receiver_name = "Hello";
+	
+	// Enter email address below for receiving the form
+	// All Contact messages will be sent there
+	$receiver_email = "example@domain.com";
+	
+	// Enter email subject below
+	// This will be your message subject
+	$msg_subject = "New Contact Message";
+	
+	$sendername = strip_tags(trim($_POST["sendername"]));
+	$sendersubject = strip_tags(trim($_POST["sendersubject"]));
+	$sendermessage = strip_tags(trim($_POST["sendermessage"]));
+    $securitycode = strip_tags(trim($_POST["securitycode"]));
+	
+	/*
+	========================================
+	Start server side validation
+	========================================
+	*/ 
+	$errors = array();
+	 //validate name
+	if(isset($_POST["sendername"])){
+	 
+			if (!$sendername) {
+				$errors[] = "Ingresa tu nombre";
+			} elseif(strlen($sendername) < 2)  {
+				$errors[] = "Nombre: Minimo 2 caracteres";
+			}
+	 
+	}
+
+	//validate subject
+	if(isset($_POST["sendersubject"])){
+			if (!$sendersubject) {
+				$errors[] = "Ingresa tu país";
+			} elseif(strlen($sendersubject) < 4)  {
+				$errors[] = "País: Minimo 2 caracteres";
+			}
+	}
+	
+	//validate message / comment
+	if(isset($_POST["sendermessage"])){
+		if (strlen($sendermessage) < 10) {
+			if (!$sendermessage) {
+				$errors[] = "Ingresa tu mensaje";
+			} else {
+				$errors[] = "Mensaje: Minimo 2 caracteres";
+			}
+		}
+	}
+	
+	//validate security captcha
+	if(isset($_POST["securitycode"])){
+		if (!$securitycode) {
+			$errors[] = "Ingresa el codigo";
+		} else if (md5($securitycode) != $_SESSION['smartCheck']['securitycode']) {
+			$errors[] = "El codigo es incorrecto";
+		}
+	}
+	
+	if ($errors) {
+		//Output errors in a list
+		$errortext = "";
+		foreach ($errors as $error) {
+			$errortext .= '<li>'. $error . "</li>";
+		}
+	
+		echo '<div class="alert notification alert-error">Ha ocurrido los siguientes errores:<br><ul>'. $errortext .'</ul></div>';
+	
+	} else{
+
+        $nombre=$sendername;
+        $pais=$sendersubject;
+        $mensaje=$sendermessage;
+        $fecha=$fechaActual;
+        $estado="D";
+
+        $rst_saludo=mysql_query("INSERT INTO iev_saludos (nombre, contenido, pais, estado_saludo, fecha) VALUES('$nombre', '$pais', '$mensaje', '$estado', '$fecha')", $conexion);
+
+        if (mysql_errno()!=0){
+            echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
+            mysql_close($conexion);
+            echo '<div class="alert notification alert-error">Ha ocurrido un error al enviar el mensaje.</div> ';
+        } else {
+            mysql_close($conexion);
+            echo '<div class="alert notification alert-success">Tu mensaje ha sido enviado.</div> ';
+        }
+	}
+?>
