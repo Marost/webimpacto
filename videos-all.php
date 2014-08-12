3f4dbac6278fd1ae9496473d60e7bdd2ee106504
Marco Lopez <mlopez@marostdevelopers.com>
2014-08-12 11:45:58 -0500

    edicion de seccion de videos con responsive

--- videos-all.php
+++ videos-all.php
@@ -21,14 +21,11 @@ require("libs/pagination/class_pagination.php");
 //INICIO DE PAGINACION
 $page           = (isset($_GET['page'])) ? intval($_GET['page']) : 1;
 $rst_noticias   = mysql_query("SELECT COUNT(*) as count FROM iev_videos WHERE publicar=1 ORDER BY fecha_publicacion DESC;", $conexion);
-//$row            = mysql_fetch_assoc($rst_noticias);
 $generated      = intval($row['count']);
-//$pagination     = new Pagination("10", $generated, $page, $url_web."?page", 1, 0);
-$pagination     = new Pagination("10", 70, $page, $url_web."?page", 1, 0);
+$pagination     = new Pagination("18", 70, $page, $url_web."?page", 1, 0);
 $start          = $pagination->prePagination();
-$rst_videos=mysql_query("SELECT * FROM iev_videos WHERE fecha_publicacion<='$fechaActual' and publicar=1 ORDER BY fecha_publicacion DESC LIMIT $start, 10", $conexion);
-//$rst_noticias   = mysql_query("SELECT * FROM iev_galeria WHERE fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC LIMIT $start, 10", $conexion);
-//$rst_videos=mysql_query("SELECT * FROM iev_videos WHERE publicar=1 ORDER BY fecha_publicacion DESC LIMIT 15;", $conexion);
+$rst_videos=mysql_query("SELECT * FROM iev_videos WHERE fecha_publicacion<='$fechaActual' and publicar=1 ORDER BY fecha_publicacion DESC LIMIT $start, 18", $conexion);
+
 ?>
 
 <!DOCTYPE html>
@@ -49,13 +46,13 @@ $rst_videos=mysql_query("SELECT * FROM iev_videos WHERE fecha_publicacion<='$fec
         <?php require_once("w-script.php"); ?>
         
         <!-- Magnific Popup core CSS file -->
-    	<link rel="stylesheet" href="css/magnific-popup.css"> 
+    	<link rel="stylesheet" href="libs/magnific-popup/css/magnific-popup.css">
 
     	<!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
     	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
 
     	<!-- Magnific Popup core JS file -->
-    	<script src="js/jquery.magnific-popup.js"></script> 
+    	<script src="libs/magnific-popup/js/jquery.magnific-popup.js"></script>
              
         <script type="text/javascript">
     	$(document).ready(function() {
@@ -118,32 +115,36 @@ $rst_videos=mysql_query("SELECT * FROM iev_videos WHERE fecha_publicacion<='$fec
 
                 <section id="nws">
 
-                    <div class="nwder notawizq">
+                    <div class="nwder col-lg-8 col-md-8 col-sm-7 col-xs-12 notawizq">
 
-                        <section class="galeria">
+                        <section class="galeria col-lg-12 col-sm-12 col-xs-12">
 
                             <h3>Videos</h3>
 
-        		    <?php while($fila_videos=mysql_fetch_array($rst_videos)){
-			            $videos_titulo=$fila_videos["titulo"];
-		                    $videos_contenido=$fila_videos["contenido"];
-		                    $videos_imagen=$fila_videos["imagen"];
-		                    $videos_imagen_carpeta=$fila_videos["imagen_carpeta"];
-		                    $videos_video=$fila_videos["video"];
+                            <?php while($fila_videos=mysql_fetch_array($rst_videos)){
+                                $videos_titulo=$fila_videos["titulo"];
+                                $videos_contenido=$fila_videos["contenido"];
+                                $videos_imagen=$fila_videos["imagen"];
+                                $videos_imagen_carpeta=$fila_videos["imagen_carpeta"];
+                                $videos_video=$fila_videos["video"];
 
-                		    //URL
-		                    $videos_UrlImg=$web."imagenes/upload/".$videos_imagen_carpeta."thumb/".$videos_imagen;
-		                    $videos_UrlYoutube="http://www.youtube.com/watch?v=".$videos_video;
-
+                                //URL
+                                $videos_UrlImg=$web."imagenes/upload/".$videos_imagen_carpeta."thumb/".$videos_imagen;
+                                $videos_UrlYoutube="http://www.youtube.com/watch?v=".$videos_video;
                             ?>
+                            <article class="col-lg-4 col-md-4 col-sm-6">
 
-                            <article>
-                                
-                                <div class="imagen">
-                                <a class="popup-youtube" href="<?php echo $videos_UrlYoutube; ?>" title="<?php echo $videos_titulo; ?>" >
-                                <img src="<?php echo $videos_UrlImg; ?>" alt="<?php echo $videos_titulo; ?>" width="290" height="210">
-                                        <h2 class="text"><?php echo $videos_titulo; ?></h2>
+                                <div class="imagen col-lg-12">
+                                    <a class="popup-youtube" href="<?php echo $videos_UrlYoutube; ?>" title="<?php echo $videos_titulo; ?>" >
+                                        <img src="<?php echo $videos_UrlImg; ?>" alt="<?php echo $videos_titulo; ?>">
                                     </a>
+                                    <div class="titulo">
+                                        <h2 class="text">
+                                            <a class="popup-youtube" href="<?php echo $videos_UrlYoutube; ?>" title="<?php echo $videos_titulo; ?>" >
+                                                <?php echo $videos_titulo; ?>
+                                            </a>
+                                        </h2>
+                                    </div>
                                 </div>
 
                             </article>
@@ -158,7 +159,7 @@ $rst_videos=mysql_query("SELECT * FROM iev_videos WHERE fecha_publicacion<='$fec
                         
                     </div>
 
-                    <div class="nwizq notawder">
+                    <div class="nwizq col-lg-4 col-md-4 col-sm-5 hidden-xs notawder">
 
                         <?php require_once("w-portada.php"); ?>
 
