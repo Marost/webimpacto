a6e0cb3d7b151c8b6eb239b8e42c035f3acbb608
Marco Lopez <mlopez@marostdevelopers.com>
2014-09-26 16:37:45 -0500

    agregando seccion noticias de portada

---
+++ panel@impacto/paginas/portada-noticias/s-guardar.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+include("../../conexion/conexion.php");
+include("../../conexion/funciones.php");
+require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
+
+//DECLARACION DE VARIABLES
+$nombre=$_POST["nombre"];
+$url=getUrlAmigable(eliminarTextoURL($nombre));
+$contenido=$_POST["contenido"];
+$categoria=$_POST["categoria"];
+$tipo_noticia=$_POST["tipo_noticia"];
+
+//FECHA Y HORA
+$pub_fecha=$_POST["pub_fecha"];
+$pub_hora=$_POST["pub_hora"];
+$fecha_publicacion=$pub_fecha." ".$pub_hora;
+$publicar=1;
+
+//TAGS
+$tags=$_POST["tags"];
+if($tags==""){ $union_tags=0; }
+elseif($tags<>""){ $union_tags=implode(",", $tags);}
+
+//SUBIR IMAGEN
+$upload_imagen=$_POST["uploader_0_tmpname"];
+
+//SUBIR VIDEO
+$video_youtube=$_POST["video_youtube"];
+$video_upload=$_POST["uploader_video_0_tmpname"];
+
+//IMAGEN
+if ($tipo_noticia=="not_destacada") {
+	$destacada=1; 
+	if($upload_imagen<>""){
+		$imagen=$upload_imagen;
+		$imagen_carpeta=fechaCarpeta()."/";	
+		$mostrar_imagen=1;
+
+        guardarImagen($imagen, $imagen_carpeta);
+
+    }else{
+		$imagen=""; $imagen_carpeta="";
+	}
+}elseif($tipo_noticia=="not_normal"){
+    $destacada=0;
+	if($upload_imagen<>""){
+		$imagen=$upload_imagen;
+		$imagen_carpeta=fechaCarpeta()."/";	
+		$mostrar_imagen=1;
+
+        guardarImagen($imagen, $imagen_carpeta);
+
+	}else{
+		$imagen=""; $imagen_carpeta="";
+	}
+}
+
+//VIDEO YOUTUBE
+if($video_youtube<>""){
+	$mostrar_video=1;
+	$tipo_video="youtube";
+	$video=$video_youtube;
+	$video_carpeta="";
+}elseif($video_youtube==""){
+	$mostrar_video=0;
+	$tipo_video="";
+	$video="";
+	$video_carpeta="";
+}
+
+//INSERTANDO DATOS
+$rst_guardar=mysql_query("INSERT INTO ".$tabla_suf."_noticia (url, titulo, contenido, imagen, imagen_carpeta, fecha_publicacion, publicar, destacada, categoria, tags, video, tipo_video, mostrar_video) VALUES('$url', '".htmlspecialchars($nombre)."', '$contenido', '$imagen', '$imagen_carpeta', '$fecha_publicacion', $publicar, $destacada, $categoria, '0,$union_tags,0', '$video', '$tipo_video', '$mostrar_video');",$conexion);
+
+if (mysql_errno()!=0){
+	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
+	mysql_close($conexion);
+	header("Location:lista.php?msj=er");
+} else {
+	mysql_close($conexion);
+	header("Location:lista.php?msj=ok");
+}
+
+?>
