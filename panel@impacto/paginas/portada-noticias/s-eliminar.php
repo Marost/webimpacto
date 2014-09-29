2275accb6f711b9a57e00870612efa7f003d1661
Marco Lopez <mlopez@marostdevelopers.com>
2014-09-29 12:43:19 -0500

    eliminacion de noticia de portada

--- panel@impacto/paginas/portada-noticias/s-eliminar.php
+++ panel@impacto/paginas/portada-noticias/s-eliminar.php
@@ -4,18 +4,21 @@ include("../../conexion/conexion.php");
 include("../../conexion/funciones.php");
 require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
 
+//VARIABLES DE URL
+$Req_Not=$_REQUEST["not"];
+
 //DECLARACION DE VARIABLES
 $id=$_REQUEST["id"];
 
-mysql_query("DELETE FROM ".$tabla_suf."_noticia WHERE id=$id;", $conexion);
+mysql_query("DELETE FROM ".$tabla_suf."_edicion_noticia WHERE id=$id;", $conexion);
 
 if (mysql_errno()!=0){
 	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
 	mysql_close($conexion);
-	header("Location:lista.php?msj=er");
+	header("Location:lista.php?not=$Req_Not&msj=er");
 } else {
 	mysql_close($conexion);
-	header("Location:lista.php?msj=el");
+	header("Location:lista.php?not=$Req_Not&msj=el");
 }
 
 ?>
