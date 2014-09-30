6c712b6ff589d826a27adcff19e7f4fc7b5b0939
Marco Lopez <mlopez@marostdevelopers.com>
2014-09-30 15:56:04 -0500

    enlace externo de css de FontAwesome

--- w-header-script.php
+++ w-header-script.php
@@ -1,7 +1,7 @@
 <base href="<?php echo $web; ?>"/>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/font-awesome.css" media="all">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" media="all">
 <link rel="stylesheet" href="css/owl.carousel.css" media="all">
 <link rel="stylesheet" href="css/jquery-ui.css" media="all">
 <link rel="stylesheet" href="css/reset.css" media="all">
