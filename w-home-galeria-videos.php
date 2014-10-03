21b7b84412c930fed925ae96ea348c249e4eb8cc
Marco Lopez <mlopez@marostdevelopers.com>
2014-10-03 11:45:14 -0500

    conexion a bd de galeria de videos de home

--- w-home-galeria-videos.php
+++ w-home-galeria-videos.php
@@ -1,3 +1,8 @@
+<?php
+//GALERIA DE VIDEOS
+$rst_videos=mysql_query("SELECT * FROM iev_videos WHERE publicar=1 AND fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC LIMIT 10", $conexion);
+
+?>
 <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
     <div class="widget-area-9">
         <div class="widget kopa-list-posts-thumb-big-small-widget">
@@ -5,9 +10,26 @@
                 <h3 class="widget-title">VIDEOS</h3>
             </header>
             <div class="widget-content">
-                <div class="item item-latest clearfix">
+                <div id="video" class="item item-latest clearfix">
 
-                    <img src="imagenes/prueba/video.jpg" alt=""/>
+                    <?php while($fila_videos=mysql_fetch_array($rst_videos)){
+                            $Videos_id=$fila_videos["id"];
+                            $Videos_url=$fila_videos["url"];
+                            $Videos_titulo=$fila_videos["titulo"];
+                            $Videos_imagen=$fila_videos["imagen"];
+                            $Videos_imagen_carpeta=$fila_videos["imagen_carpeta"];
+
+                            //URLS
+                            $Videos_UrlWeb=$web."video/".$Videos_id."-".$Videos_url;
+                            $Videos_UrlImg=$web."imagenes/upload/".$Videos_imagen_carpeta."".$Videos_imagen;
+                    ?>
+                    <div class="slide">
+                        <a href="<?php echo $Videos_UrlWeb; ?>">
+                        <span><p><?php echo $Videos_titulo; ?></p></span>
+                            <img src="<?php echo $Videos_UrlImg; ?>" alt="<?php echo $Videos_titulo; ?>" width="300" height="182"/>
+                        </a>
+                    </div>
+                    <?php } ?>
 
                 </div>
                 <!-- item latest -->
