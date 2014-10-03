5fbc03a77ccfacfd7f5d1c06200cc4160508e1a4
Marco Lopez <mlopez@marostdevelopers.com>
2014-10-03 11:12:36 -0500

    css de galeria de videos con bxslider

--- w-header-script.php
+++ w-header-script.php
@@ -7,15 +7,20 @@
 <link rel="stylesheet" href="css/reset.css" media="all">
 <link rel="stylesheet" href="css/grid.css" media="all">
 <link rel="stylesheet" href="css/superfish.css" media="all">
-<link rel="stylesheet" href="css/bxslider.css" media="all"/>
+<!--<link rel="stylesheet" href="css/bxslider.css" media="all"/>-->
 <link rel="stylesheet" href="css/navgoco.css" media="all">
 <link rel="stylesheet" href="css/style.css" media="all">
 <link rel="stylesheet" href="css/responsive.css" media="all">
 
 <?php if(isset($w_galeria)){ if($w_galeria==true){  ?>
-<!-- GALERIA -->
+<!-- GALERIA DE FOTOS -->
 <link href="libs/royalslider/royalslider.css" rel="stylesheet">
 <link href="libs/royalslider/skins/default/rs-default.css" rel="stylesheet">
+<?php }} ?>
+
+<?php if(isset($w_video)){ if($w_video==true){  ?>
+<!-- GALERIA DE VIDEOS -->
+<link rel="stylesheet" href="libs/bxslider/css/jquery.bxslider.css"/>
 <?php }} ?>
 
 <script src="js/modernizr.custom.js"></script>
