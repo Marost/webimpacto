ebba2ceef520f4f5518b56b29c59f11ce274f0ea
Marco Lopez <mlopez@marostdevelopers.com>
2014-10-13 15:59:14 -0500

    estilos css con url de tiempo

--- w-header-script.php
+++ w-header-script.php
@@ -2,32 +2,33 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" media="all">
-<link rel="stylesheet" href="css/owl.carousel.css" media="all">
-<link rel="stylesheet" href="css/jquery-ui.css" media="all">
-<link rel="stylesheet" href="css/reset.css" media="all">
-<link rel="stylesheet" href="css/grid.css" media="all">
-<link rel="stylesheet" href="css/superfish.css" media="all">
-<!--<link rel="stylesheet" href="css/bxslider.css" media="all"/>-->
-<link rel="stylesheet" href="css/navgoco.css" media="all">
-<link rel="stylesheet" href="css/style.css" media="all">
-<link rel="stylesheet" href="css/responsive.css" media="all">
+<link rel="stylesheet" href="css/owl.carousel.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/jquery-ui.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/reset.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/grid.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/superfish.css?update=<?php echo date("YmdHi") ?>" media="all">
+<!--<link rel="stylesheet" href="css/bxslider.css?update=<?php echo date("YmdHi") ?>" media="all"/>-->
+<link rel="stylesheet" href="css/navgoco.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/style.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/responsive.css?update=<?php echo date("YmdHi") ?>" media="all">
+<link rel="stylesheet" href="css/responsivo.css?update=<?php echo date("YmdHi") ?>" media="all">
 
 <?php if(isset($w_galeria)){ if($w_galeria==true){  ?>
 <!-- GALERIA DE FOTOS -->
-<link href="libs/royalslider/royalslider.css" rel="stylesheet">
-<link href="libs/royalslider/skins/default/rs-default.css" rel="stylesheet">
+<link href="libs/royalslider/royalslider.css?update=<?php echo date("YmdHi") ?>" rel="stylesheet">
+<link href="libs/royalslider/skins/default/rs-default.css?update=<?php echo date("YmdHi") ?>" rel="stylesheet">
 <?php }} ?>
 
 <?php if(isset($w_video)){ if($w_video==true){  ?>
 <!-- GALERIA DE VIDEOS -->
-<link rel="stylesheet" href="libs/bxslider/css/jquery.bxslider.css"/>
+<link rel="stylesheet" href="libs/bxslider/css/jquery.bxslider.css?update=<?php echo date("YmdHi") ?>"/>
 <?php }} ?>
 
 <!-- POPUP -->
-<link rel="stylesheet" href="libs/magnific-popup/css/magnific-popup.css"/>
+<link rel="stylesheet" href="libs/magnific-popup/css/magnific-popup.css?update=<?php echo date("YmdHi") ?>"/>
 
 <!-- FORMULARIO SALUDO-->
-<link rel="stylesheet" href="libs/smartform/css/smart-forms.css"/>
-<!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="libs/smartform/css/smart-forms-ie8.css"><![endif]-->
+<link rel="stylesheet" href="libs/smartform/css/smart-forms.css?update=<?php echo date("YmdHi") ?>"/>
+<!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="libs/smartform/css/smart-forms-ie8.css?update=<?php echo date("YmdHi") ?>"><![endif]-->
 
 <script src="js/modernizr.custom.js"></script>
