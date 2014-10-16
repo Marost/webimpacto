4db31d8ca7303ffeb7d54cf6fffe6a79b2f30639
Marco Lopez <mlopez@marostdevelopers.com>
2014-10-16 12:15:11 -0500

    clases de bootstrap para diseño responsivo en noticias principal

--- w-home-noticias-prin.php
+++ w-home-noticias-prin.php
@@ -55,13 +55,13 @@ $rst_notNor=mysql_query("SELECT * FROM iev_noticia WHERE destacada=0 AND categor
                             <?php echo $NotDest_contenido; ?>
                         </div>
 
-                        <div class="kopa-metadata-border col-lg-12 col-md-12">
+                        <div class="kopa-metadata-border col-lg-12 col-md-12 col-sm-12">
 
                             <!-- AddThis -->
-                            <div class="addthis_native_toolbox col-lg-6 col-md-6"></div>
+                            <div class="addthis_native_toolbox col-lg-6 col-md-6 col-sm-6"></div>
                             <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50f364066076ff63"></script>
 
-                            <div class="notaprin-mdstar col-lg-3 col-md-3">
+                            <div class="notaprin-mdstar col-lg-3 col-md-3 col-sm-3">
                                 <span class="kopa-rate">
                                     <i class="fa fa-star"></i>
                                     <i class="fa fa-star"></i>
@@ -71,7 +71,7 @@ $rst_notNor=mysql_query("SELECT * FROM iev_noticia WHERE destacada=0 AND categor
                                 </span>
                             </div>
 
-                            <div class="notaprin-mdln col-lg-3 col-md-3">
+                            <div class="notaprin-mdln col-lg-3 col-md-3 col-sm-3">
                                 <a href="<?php echo $NotDest_UrlWeb; ?>" class="kopa-readmore">Leer nota</a>
                             </div>
 
