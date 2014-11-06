d03696cb25fc697d59b8743bfb1fe6a4e3437cd4
Marco Lopez <mlopez@marostdevelopers.com>
2014-11-06 12:18:46 -0500

    imagenes y enlaces en footer

--- w-footer.php
+++ w-footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="widget-area-14">
                     <div class="widget kopa-ads-widget">
                         <div class="widget-content">
@@ -20,7 +20,41 @@
                     </div>
                 </div>
             </div>
-            <!-- col 3 -->
+
+            <div class="col-lg-3 col-md-3 hidden-sm hidden-xs"></div>
+
+            <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12">
+                <div class="widget-area-16">
+                    <div class="widget kopa-tweet-widget">
+                        <header class="widget-header">
+                            <h3 class="widget-title">Enlaces</h3>
+                        </header>
+                        <div id="footer-enlaces" class="widget-content">
+                            <ul>
+                                <li class="col-lg-5">
+                                    <div class="col-lg-12">
+                                        <a target="_blank" href="http://betheltv.tv/" title="Bethel TV">
+                                            <img height="70" src="imagenes/logos/logo-bethel-color.png" alt="Bethel TV">
+                                        </a>
+                                        <p>Bethel TV</p>                                        
+                                    </div>                                    
+                                </li>
+                                <li class="col-lg-5">
+                                    <div class="col-lg-12">
+                                        <a target="_blank" href="http://mmmperu.org/" title="Movimiento Misionero Mundial">
+                                            <img height="70" src="imagenes/logos/logo-mmm-color.png" alt="Movimiento Misionero Mundial">
+                                        </a>
+                                        <p>Movimiento Misionero Mundial</p>                                        
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- widget-content -->
+
+                    </div>
+                    <!-- kp-tweet -->
+                </div>
+            </div>
 
         </div>
         <!-- row -->
