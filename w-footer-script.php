7036ce6ac514bc97d2ac0aa7c0e944e2098de4d3
Marco Lopez <mlopez@marostdevelopers.com>
2014-11-06 21:52:14 -0500

    agregando script de Google Analytics

--- w-footer-script.php
+++ w-footer-script.php
@@ -20,3 +20,12 @@
 
 <!-- ADDTHIS -->
 <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50f364066076ff63"></script>
+
+<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-20229980-10', 'impactoevangelistico.net');
+  ga('send', 'pageview');
+</script>
