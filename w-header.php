c995e26551827ba9db857ead4f200a3536a467a4
Marco Lopez <mlopez@syscontrollers.com>
2018-07-08 15:24:51 -0500

    header

--- w-header.php
+++ w-header.php
@@ -67,6 +67,7 @@ $UrlWeb_AW="http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
                             <li><a href="edicion-anterior-ch"><img src="imagenes/flags/ch.png" alt="Chino"/> Chino</a></li>
                         </ul>
                     </li>
+                    <li><a href="#">Edición Europa</a></li>
                     <li><a href="javascript:;">Secciones</a>
                         <ul>
                             <li><a href="categoria/11/portada">Portada</a></li>
@@ -98,6 +99,7 @@ $UrlWeb_AW="http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
                             <li><a href="edicion-anterior-ch"><img src="imagenes/flags/ch.png" alt="Chino"/> Chino</a></li>
                         </ul>
                     </li>
+                    <li><a href="#">Edición Europa</a></li>
                     <li><a href="javascript:;">Secciones</a>
                         <ul>
                             <li><a href="categoria/11/portada">Portada</a></li>
